32721a8f4895cb1f3d20e9e6845c50f9eb370263
Kevin Daysal <kdaysal123@gmail.com>
2022-07-27 19:44:41 -0400

    Created UserLoginFormComponent to log the user in and write 'token' and 'user' to localStorage

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,5 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
+// This import will be used to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import brings in the API calls I created in 6.2
+import { UserRegistrationService } from '../fetch-api-data.service';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+//Decorator containing instructions to wire up up the class with its stylesheet and template file
+//the selector property defines the custom HTML element into which this component will render
+//...my form component can be used elsewhere as follows: <app-user-registration-form></app-user-registration-form>
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
@@ -7,9 +19,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginFormComponent implements OnInit {
 
-  constructor() { }
+  //Decorator to define the component's input / output
+  @Input() userData = { Username: '', Password: '' };
 
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+  //this method is called once the component has received all its inputs (i.e. all its data-bound properties) from the user
   ngOnInit(): void {
+  }
+
+  // This is the function responsible for sending the form inputs to the backend
+  loginUser(): void {
+    console.log(`loginUser() running`);
+    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+      // Logic for a successful user login goes here! (To be implemented)
+      this.dialogRef.close(); // This will close the modal on success!
+      console.log(`result: ${result}`);
+      //update localStorage with token and username
+      localStorage.setItem('token', result.token);
+      localStorage.setItem('user', result.user.Username);
+
+      this.snackBar.open(result, 'OK, user was logged in!', {
+        duration: 2000
+      });
+    }, (result) => {
+      console.log(`result: ${result}`);
+      this.snackBar.open(result, 'OK, user was logged in!!', {
+        duration: 2000
+      });
+    });
   }
 
 }
