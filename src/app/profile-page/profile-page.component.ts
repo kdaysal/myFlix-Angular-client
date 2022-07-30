02e32e00552d5254f64c48e98e911678c5e84cab
Kevin Daysal <kdaysal123@gmail.com>
2022-07-30 15:53:11 -0400

    First attempt at building a basic profile page to render user profile details.
    -This is not working yet - I'm getting a 404 on the GET call to my .../users/User48 endpoint

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+//import { editProfileCompoment } from '../edit-profile/edit-profile.component';
 
 @Component({
   selector: 'app-profile-page',
@@ -6,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-page.component.scss']
 })
 export class ProfilePageComponent implements OnInit {
-
-  constructor() { }
+  user: any = {};
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialog: MatDialog,
+    public router: Router,
+    public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.getUser();
   }
+
+  getUser(): void {
+    const username = localStorage.getItem('user');
+    this.fetchApiData.getUser(username).subscribe((resp: any) => {
+      this.user = resp;
+      console.log(`user: ${this.user}`);
+      return this.user;
+    })
+  }
+
+  //TBD - code to open the EditProfileDialog here...
+
 
 }
