eeb1d005d8956a7dc66cf9bb1e11d33c35d4f352
Kevin Daysal <kdaysal123@gmail.com>
2022-07-31 20:47:31 -0400

    Initial buildout of EditProfileComponent to allow user to edit their profile data
    -TODO - implement logic for editUser() function

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.component.scss']
 })
 export class EditProfileComponent implements OnInit {
+  @Input() userData: any = {};
 
-  constructor() { }
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialog: MatDialog,
+    public router: Router,
+    public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
-}
+  //implement editUser() function here...
+  editUser(): void {
+    console.log(`editUser() called`);
+
+  }
+
+}//end EditProfileComponent
