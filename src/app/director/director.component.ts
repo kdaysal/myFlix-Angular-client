3312f6dc15f3984119b46d548b11617e99d9adad
Kevin Daysal <kdaysal123@gmail.com>
2022-08-03 00:41:44 -0400

    FIXED!! - Finally able to pass director details from openDirectorDialog() in movie-card.component.ts to my director.component.ts and director.component.html files
    -The first compile error was misleading me into thinking there was a problem with how DirectComponent was declared in my app.module.ts file, but I later realized the issue was due to leaving 2 public declarations (for dialogRef and fetchApiData) right below my @Inject(MAT_DIALOG_DATA) statement and before declaring my 'data' object. I put those declarations there before when I was attempting to do an API call, and that's what messed up my first several attempts to use @Inject :).

--- src/app/director/director.component.ts
+++ src/app/director/director.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { UserRegistrationService } from '../fetch-api-data.service'
 
 
@@ -9,26 +9,39 @@ import { UserRegistrationService } from '../fetch-api-data.service'
   styleUrls: ['./director.component.scss']
 })
 export class DirectorComponent implements OnInit {
-  director: any = {};
+  director: any = {};//may not be necessary anymore - delete before production
 
   constructor(
-    public dialogRef: MatDialogRef<DirectorComponent>,
-    public fetchApiData: UserRegistrationService,
+    @Inject(MAT_DIALOG_DATA)
+    /* CANNOT have the 2 lines below...this is where the compile errors came from */
+    //public dialogRef: MatDialogRef<DirectorComponent>,
+    //public fetchApiData: UserRegistrationService,
+    public data: {
+      Name: string,
+      Bio: string,
+      Birth: Date,
+      Death: Date
+    }
 
   ) { }
 
   ngOnInit(): void {
+    /* NO LONGER NECESSARY TO CALL getDirector endpoint - delete before production */
     //call to get director info here, if @Inject won't work
     // this.getDirector("Quentin%20Tarantino"); //to test the API call with a hard-coded director name
   }
 
-  getDirector(name: string): void {
-    this.fetchApiData.getDirector(name).subscribe((resp: any) => {
-      this.director = resp;
-      console.log(this.director);
-      return this.director;
-    });
-  }
+  /* NO LONGER NECESSARY TO CALL getDirector endpoint...I finally got the @injector to work */
+  /* DELETE BELOW CODEBLOCK before production */
+  /* ********************************** */
+  // getDirector(name: string): void {
+  //   this.fetchApiData.getDirector(name).subscribe((resp: any) => {
+  //     this.director = resp;
+  //     console.log(this.director);
+  //     return this.director;
+  //   });
+  // }
+  /* ********************************** */
 
 
 }
